18bed4c808f5ec53865606b35e51c711b53904bb
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2024-08-15 17:45:38 +0700

    Tour: Adding Serilog

--- src/Services/Tour/Tour.API/Program.cs
+++ src/Services/Tour/Tour.API/Program.cs
@@ -1,25 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+using BuildingBlocks.Logging;
+using Serilog;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog(Serilogger.Configure);
+
+    Log.Information($"Starting {builder.Environment.ApplicationName} up");
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
