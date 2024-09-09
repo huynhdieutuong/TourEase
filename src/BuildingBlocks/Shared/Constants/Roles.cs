1975ed8b83bea168f98b7c7a916b93f5fb27e724
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2024-09-09 20:54:55 +0700

    IdentityServer: Add Roles

---
+++ src/BuildingBlocks/Shared/Constants/Roles.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Shared.Constants;
+public class Roles
+{
+    public const string Admin = "Admin";
+    public const string TravelAgency = "TravelAgency";
+    public const string TourGuide = "TourGuide";
+}
--- src/Services/Identity/IdentityServer/Services/CustomProfileService.cs
+++ src/Services/Identity/IdentityServer/Services/CustomProfileService.cs
@@ -1,6 +1,5 @@
 ﻿using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services;
-using IdentityModel;
 using IdentityServer.Models;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
@@ -19,15 +18,16 @@ public class CustomProfileService : IProfileService
     public async Task GetProfileDataAsync(ProfileDataRequestContext context)
     {
         var user = await _userManager.GetUserAsync(context.Subject);
-        var existingClaims = await _userManager.GetClaimsAsync(user);
+        var roles = await _userManager.GetRolesAsync(user);
 
         var claims = new List<Claim>
         {
-            new Claim("username", user.UserName)
+            new Claim("username", user.UserName),
+            new Claim("roles", string.Join(",", roles))
         };
 
         context.IssuedClaims.AddRange(claims);
-        context.IssuedClaims.Add(existingClaims.FirstOrDefault(x => x.Type == JwtClaimTypes.Name));
+        //context.IssuedClaims.Add(existingClaims.FirstOrDefault(x => x.Type == JwtClaimTypes.Name));
     }
 
     public Task IsActiveAsync(IsActiveContext context)
