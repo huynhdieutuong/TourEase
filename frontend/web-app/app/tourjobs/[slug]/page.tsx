6ee1470d7a8e7df622ab998e14b8b43ecfef1608
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2024-09-21 21:28:13 +0700

    Auth: Protect Routes with Role-based Access

---
+++ frontend/web-app/app/tourjobs/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TourJobDetails from './TourJobDetails'
+
+export default function DetailsBySlug({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  return <div>DetailsBySlug</div>
+}
--- frontend/web-app/middleware.ts
+++ frontend/web-app/middleware.ts
@@ -1,8 +1,40 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getCurrentUser } from './app/actions/authActions'
+import { Role } from './types/enums'
+import { getToken } from 'next-auth/jwt'
+
 export { auth as middleware } from '@/auth'
+
+// export default middleware((req: NextRequest) => {
+//   const url = req.nextUrl.pathname
+//   const token = getToken()
+
+//   if (url.startsWith('/destinations') && user?.roles.includes(Role.ADMIN)) {
+//     return NextResponse.redirect(new URL('/403', req.url))
+//   }
+
+//   if (url.startsWith('/tourjobs') && user?.roles.includes(Role.TRAVELAGENCY)) {
+//     return NextResponse.redirect(new URL('/403', req.url))
+//   }
+
+//   return NextResponse.next()
+// })
 
 export const config = {
   // protect routes
-  matcher: ['/session'],
+  matcher: [
+    '/session',
+
+    '/tourjobs/list',
+    '/tourjobs/create',
+    '/tourjobs/details/:path*',
+    '/tourjobs/update/:path*',
+
+    '/destinations/list',
+    '/destinations/create',
+    '/destinations/details/:path*',
+    '/destinations/update/:path*',
+  ],
 
   // override pages
   pages: { signIn: '/api/auth/signin' },
