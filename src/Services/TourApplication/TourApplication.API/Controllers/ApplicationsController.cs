a25b28f6beaea879f4c84f8a45f1799ac42dc2a5
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2024-09-30 22:14:38 +0700

    TourApplication: Containerize and Update Document

--- src/Services/TourApplication/TourApplication.API/Controllers/ApplicationsController.cs
+++ src/Services/TourApplication/TourApplication.API/Controllers/ApplicationsController.cs
@@ -31,13 +31,6 @@ public class ApplicationsController : ControllerBase
     [Authorize(Roles = Roles.TourGuide)]
     public async Task<ActionResult<ApiResult<ApplicationDto>>> ApplyTourJob(ApplyTourJobCommand command)
     {
-        // One TourGuide only apply TourJob once time
-        // Only TourGuide role can Apply
-        // In case user have 2 role (TourGuide and TravelAgency), need check Owner can't apply
-        // Can't apply for a expired or finished job
-        // Default Status of Application is Pending
-
-        // Publish event to Tour and TourSearch to update TotalApplicants (only count <> Cancel)
         command.Username = User.Identity.Name;
         var result = await _mediator.Send(command);
         return Ok(result);
@@ -56,11 +49,6 @@ public class ApplicationsController : ControllerBase
     [Authorize(Roles = Roles.TourGuide)]
     public async Task<ActionResult<ApiResult<bool>>> CancelApplication(Guid applicationId)
     {
-        // Ony the TourGuide can cancel their Applications
-        // Only Pending Status can change to Cancel
-        // need check TourJob Status is complete or not
-
-        // Publish event to Tour and TourSearch to update TotalApplicants
         var command = new CancelApplicationCommand(applicationId, User.Identity.Name);
         var result = await _mediator.Send(command);
         return Ok(result);
@@ -70,11 +58,6 @@ public class ApplicationsController : ControllerBase
     [Authorize(Roles = Roles.TourGuide)]
     public async Task<ActionResult<ApiResult<bool>>> ReApplyApplication(Guid applicationId)
     {
-        // Ony the TourGuide can re-apply their Applications
-        // Only Cancel Status can change to Pending (ReApply)
-        // need check TourJob Status is complete or not
-
-        // Publish event to Tour and TourSearch to update TotalApplicants
         var command = new ReApplyApplicationCommand(applicationId, User.Identity.Name);
         var result = await _mediator.Send(command);
         return Ok(result);
@@ -84,15 +67,8 @@ public class ApplicationsController : ControllerBase
     [Authorize(Roles = Roles.TravelAgency)]
     public async Task<ActionResult<ApiResult<bool>>> ChooseTourGuide(Guid applicationId)
     {
-        // Only the TravelAgency can choose TourGuide on their TourJobs
-        // Only choose Pending Application
-        // need check TourJob Status is complete or not
-        // Update Status of Applications (All Status is Rejected, one is Accepted?)
-        // Update TourGuide in TourJob
         var command = new ChooseTourGuideCommand(applicationId, User.Identity.Name);
         var result = await _mediator.Send(command);
         return Ok(result);
     }
-
-    // Consume Delete TourJob, will delete all Applications as well
 }
